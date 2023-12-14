fe7281ca56360860fd62766347294a688e284b05
dcharistes <d.charistes@gmail.com>
2023-12-14 18:33:24 +0200

    ok

--- FINAL/DFA.m
+++ FINAL/DFA.m
@@ -1,38 +1,32 @@
 function [output1,output2,output3,output4]=DFA(DATA,win_length,order)
-      
-       N=length(DATA);  %7168 
-       n=floor(N/win_length); %71.00
-       N1=n*win_length; %7100
-       y=zeros(N1,1);
-       Yn=zeros(N1,1);
-       
-       fitcoef=zeros(n,order+1);
-       
-            mean1=mean(DATA(1:N1));
-            %plot(1:N1,mean1);
-       for i=1:N1
-               
-                y(i)=sum(DATA(1:i)-mean1);
-       end
 
-       
-            y=y';
-          
-            for j=1:n
+N=length(DATA);  %7168 
+n=floor(N/win_length); %71.00
+N1=n*win_length; %7100
+y=zeros(N1,1);
+Yn=zeros(N1,1);
 
-                fitcoef(j,:)=polyfit(1:win_length,y(((j-1)*win_length+1):j*win_length),order);
-            end
- 
-            for j=1:n
-                Yn(((j-1)*win_length+1):j*win_length)=polyval(fitcoef(j,:),1:win_length);
-               
-            end
+fitcoef=zeros(n,order+1);
 
-            
-           
-            sum1=sum((y'-Yn).^2)/N1;
-            sum1=sqrt(sum1);
-            output1=sum1;
-            output2=y;
-            output3=Yn;
-          output4=N1;
+mean1=mean(DATA(1:N1));
+%plot(1:N1,mean1);
+for i=1:N1   
+    y(i)=sum(DATA(1:i)-mean1);
+end
+    
+y=y';
+  
+for j=1:n
+    fitcoef(j,:)=polyfit(1:win_length,y(((j-1)*win_length+1):j*win_length),order);
+end
+
+for j=1:n
+    Yn(((j-1)*win_length+1):j*win_length)=polyval(fitcoef(j,:),1:win_length);   
+end
+
+sum1=sum((y'-Yn).^2)/N1;
+sum1=sqrt(sum1);
+output1=sum1;
+output2=y;
+output3=Yn;
+output4=N1;
--- FINAL/DFA_call_F.m
+++ FINAL/DFA_call_F.m
@@ -79,42 +79,37 @@ return
 end
 
 
-function [output1,output2,output3,output4]=DFA(DATA,win_length,order)
+function [out1,out2,out3,out4]=DFA(DATA,win_length,order)
       
-       N=length(DATA);  %7168 
-       n=floor(N/win_length); %71.00
-       N1=n*win_length; %7100
-       y=zeros(N1,1);
-       Yn=zeros(N1,1);
-       
-       fitcoef=zeros(n,order+1);
-       
-            mean1=mean(DATA(1:N1));
-            %plot(1:N1,mean1);
-       for i=1:N1
-               
-                y(i)=sum(DATA(1:i)-mean1);
-       end
+N=length(DATA);  %7168 
+n=floor(N/win_length); %71.00
+N1=n*win_length; %7100
+y=zeros(N1,1);
+Yn=zeros(N1,1);
 
-       
-            y=y';
-          
-            for j=1:n
+fitcoef=zeros(n,order+1);
 
-                fitcoef(j,:)=polyfit(1:win_length,y(((j-1)*win_length+1):j*win_length),order);
-            end
- 
-            for j=1:n
-                Yn(((j-1)*win_length+1):j*win_length)=polyval(fitcoef(j,:),1:win_length);
-               
-            end
+mean1=mean(DATA(1:N1));
+%plot(1:N1,mean1);
+for i=1:N1
+    y(i)=sum(DATA(1:i)-mean1);
+end
 
-            
-           
-            sum1=sum((y'-Yn).^2)/N1;
-            sum1=sqrt(sum1);
-            output1=sum1;
-            output2=y;
-            output3=Yn;
-          output4=N1;
+y=y';
+
+for j=1:n
+    fitcoef(j,:)=polyfit(1:win_length,y(((j-1)*win_length+1):j*win_length),order);
+end
+
+for j=1:n
+    Yn(((j-1)*win_length+1):j*win_length)=polyval(fitcoef(j,:),1:win_length);  
+end
+
+sum1=sum((y'-Yn).^2)/N1;
+sum1=sqrt(sum1);
+out1=sum1; 
+out2=y;
+out3=Yn;
+out4=N1;
+
 end
