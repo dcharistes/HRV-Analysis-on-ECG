ddd6ebe98e4a0834d7597a729573d595bdd1d379
Dimitris Charistes <d.charistes@gmail.com>
2023-12-29 14:18:02 +0200

    changed t to discrete sec according to samples

--- fantasia_DFA.m
+++ fantasia_DFA.m
@@ -31,7 +31,7 @@ function [D,Alpha1]=DFA_call_fant(DATA)
 ecg=DATA;
 f_s=250;
 N=length(ecg);
-t=(0:N-1)/f_s; %time period(total sample/Fs)
+t=0:N-1; %time period(total sample/Fs)
 
 subplot(221)
 plot(t,ecg,'r'),grid on,title('Raw ECG Data plotting '),grid on;             
@@ -48,7 +48,7 @@ g=wrcoef('d',e,f,'db6',16);
 ecg_wave=ecg_notch-g; 
 ecg_smooth=smooth(ecg_wave); 
 N1=length(ecg_smooth);
-t1=(0:N1-1)/f_s;
+t1=0:N1-1;
 
 subplot(222)
 plot(t1,ecg_smooth),grid on,ylabel('amplitude'),xlabel('time')
